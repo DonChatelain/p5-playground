38951454463605a90720cab19338887901dec2b7
Don Chatelain <djchatelain@gmail.com>
2019-03-13 05:59:09 -0600

    basic setup for tutorial

--- src/sketch.js
+++ src/sketch.js
@@ -1,9 +1,20 @@
+import p5 from 'p5'
 
+const sketch = (p) => {
 
-function setup() {
-
-}
-
-function draw() {
   
+  // initializes on startup
+  p.setup = () => {
+    p.createCanvas(window.innerWidth, window.innerHeight)
+    // p.rectMode(p.CENTER)
+    p.background(190) 
+    p.rect(p.width/2, p.height/2, 50, 50)
+  }
+
+  // runs every frame
+  p.draw = () => {
+    
+  }
 }
+
+new p5(sketch, 'root')
