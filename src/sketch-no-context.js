6eff5c2b7cda0a2bacfe88d24f8fa338c384a01f
Don <don.chatelain@hoverstate.com>
2019-03-22 11:00:24 -0600

    provides an example js file with no module import / no context

---
+++ src/sketch-no-context.js
@@ -0,0 +1,11 @@
+// you can structure your setup and draw functions globally as well as access global functions and variables when you import p5.js directly into your html
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight)
+  background(190)
+  rect(width/2, height/2, 50, 50)
+}
+
+function draw() {
+
+}
