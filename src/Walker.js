cdfa963006f464414fff384ac2fef3f4a069e66b
Don Chatelain <djchatelain@gmail.com>
2022-06-09 15:55:44 -0600

    tweaks to walker

--- src/Walker.js
+++ src/Walker.js
@@ -1,28 +1,39 @@
-export default function Walker (p) {
-  this.x = p.width / 2;
-  this.y = p.height / 2;
-  this.width = 9;
-  this.height = 9;
-  this.distance = 3;
+export default function Walker (p, oX, oY) {
+  this.x = oX || p.width / 4;
+  this.y = oY || p.height / 2.5;
+  this.width = 15;
+  this.height = 15;
+  this.distance = 15;
   this.color = { r: 0, g: 0, b: 0, a: 50}
+  this.xoff = 0.0;
+  this.yoff = 0.0;
 
   this.display = () => {
     p.fill(p.color(this.color.r, this.color.g, this.color.b, this.color.a))
     p.noStroke()
-    this.width = p.map(p.mouseX, 0, p.width, 5, 10)
-    p.rect(this.x, this.y, this.width, this.width)
+    // this.width = p.map(p.mouseX, 0, p.width, 5, 10)
+    p.ellipse(this.x, this.y, this.width, this.height)
   }
 
   this.move = () => {
     const DIRECTIONS = 4
     const choice = Math.floor(Math.random() * DIRECTIONS)
+    this.xoff += Math.random() * 2;
+    this.yoff += Math.random() * 2;
+    let nX = p.noise(this.xoff) * this.width + this.distance
+    let nY = p.noise(this.yoff) * this.height + this.distance
     switch (choice) {
-      case 0: this.x += this.width + this.distance; break
-      case 1: this.y += this.width + this.distance; break
-      case 2: this.x -= this.width + this.distance; break
-      case 3: this.y -= this.width + this.distance; break
+      // case 0: this.x += this.width + this.distance; break
+      // case 1: this.y += this.width + this.distance; break
+      // case 2: this.x -= this.width + this.distance; break
+      // case 3: this.y -= this.width + this.distance; break
+      case 0: this.x += nX; break;
+      case 1: this.y += nY; break;
+      case 2: this.x -= nX; break;
+      case 3: this.y -= nY; break;
       default: break
     }
+
     this.checkBoundaries()
   }
 
--- src/sketch.js
+++ src/sketch.js
@@ -1,15 +1,23 @@
 import p5 from 'p5'
 import Walker from './Walker'
 
-const FRAME_RATE = 90;
+const FRAME_RATE = 120;
 
 const sketch = (p) => {
   let walter;
+  let wilson;
+  let wallace;
+  let willy;
+  let witley;
   
   p.setup = () => {
     p.createCanvas(window.innerWidth, window.innerHeight)
     p.frameRate(FRAME_RATE)
-    walter = new Walker(p)
+    walter = new Walker(p, p.width / 4, p.height / 2.5)
+    wilson = new Walker(p, p.width / 2, p.height / 2)
+    wallace = new Walker(p, p.width / 1.5, p.height / 1.5)
+    willy = new Walker(p, 50, 50)
+    witley = new Walker(p, p.width - 50, p.height - 50)
   }
 
   p.draw = () => {
@@ -17,6 +25,18 @@ const sketch = (p) => {
     walter.move()
     walter.shiftColor()
     walter.display()
+    wilson.move()
+    wilson.shiftColor()
+    wilson.display()
+    wallace.move()
+    wallace.shiftColor()
+    wallace.display()
+    willy.move()
+    willy.shiftColor()
+    willy.display()
+    witley.move()
+    witley.shiftColor()
+    witley.display()
   }
 
 }
